9be5fd71bf06cca80ec013192b15aad9669aa26b
gzzwinner <gzzwinner@163.com>
2017-08-24 15:41:03 +0500

    super update commit

--- src/com/imooc/animal/Animal.java
+++ src/com/imooc/animal/Animal.java
@@ -7,15 +7,32 @@ public class Animal {
 	 *默认：允许在当前类、跨包子类和非子类不能调用、同包子类/非子类可以调用
 	 */
 	
-	private String name;//昵称
-	protected int month;//年龄
-	public String species;//品种
-	
+	private String name = "妮妮";//昵称
+	protected int month = 2;//年龄
+	public String species = "动物";//品种
 	public int temp = 15;
 	
-	//父类的构造方法不允许被继承、不允许被重写//?
+	//静态成员
+	private static int st1 = 22;
+	public static int st2 = 33; 
+	
+	//静态代码块
+	static {
+		System.out.println("我是父类的静态代码块");
+	}
+	
+	//构造代码块
+	{
+		System.out.println("我是父类的构造代码块");
+	}
+	
+	//父类的构造方法不允许被继承、不允许被重写，但是会影响子类对象的实例化过程
 	public Animal(){
-		
+		System.out.println("我是父类的无参构造");
+	}
+	
+	public Animal(String name, int month) {
+		System.out.println("我是父类的带参构造");
 	}
 
 	public String getName() {
--- src/com/imooc/animal/Cat.java
+++ src/com/imooc/animal/Cat.java
@@ -5,8 +5,30 @@ public class Cat extends Animal {
 	private double weight; //体重
 	//public int temp = 300;
 	
+	public static int st3 = 44;
+	
+	//静态代码块
+	static {
+		System.out.println("我是子类的静态代码块");
+	}
+	
+	//构造代码块
+	{
+		System.out.println("我是子类的构造代码块");
+	}
+	
 	public Cat(){
-		this.month = 23;
+		//this.month = 23;
+		System.out.println("我是子类的无参构造方法");
+	}
+	
+	public Cat(String name, int month) {
+		/*子类构造默认调用的是父类的无参构造方法，
+		 * 可以通过super()调用父类允许被访问的其他构造方法
+		 * super()必须放在子类构造方法有效代码的第一行
+		*/
+		super(name, month);//调用父类的双参构造
+		System.out.println("我是子类的带参构造方法");
 	}
 
 	public double getWeight() {
